df88e9f39f2abb15c520256f527a3d0d8b2347d7
Pinto Manuel <87946535+Pinto1232@users.noreply.github.com>
2021-09-04 19:48:58 +0200

    home section
    I started building the home section

--- js/script.js
+++ js/script.js
@@ -9,3 +9,10 @@ function toggleMyNav(){
     navBarToggler.classList.toggle("active")
     document.querySelector(".nav").classList.toggle("open")
 }
+
+/**Close nav when item is clicked */
+document.addEventListener("click", function(e){
+    if (e.target.closest(".nav-item")) {
+        toggleMyNav();
+    }
+})
