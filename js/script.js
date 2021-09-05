2a6ef012fc4e55a48fc01b33fce522ef7fd8ceba
Pinto Manuel <87946535+Pinto1232@users.noreply.github.com>
2021-09-05 02:55:28 +0200

    Loader added
    loader

--- js/script.js
+++ js/script.js
@@ -15,4 +15,19 @@ document.addEventListener("click", function(e){
     if (e.target.closest(".nav-item")) {
         toggleMyNav();
     }
-})
+});
+
+
+
+/**
+ *  Loader
+ */
+ function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+  }
+  
+  function fadeOut(){
+    setInterval(loader, 3000);
+  }
+  
+  window.onload = fadeOut;
